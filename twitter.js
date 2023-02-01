56b96d5af88d00d2c1d3b8bd33be8c26940818bc
Steven M <>
2023-02-01 19:37:23 +0000

    Update Twitter dependency

--- twitter.js
+++ twitter.js
@@ -1,20 +1,23 @@
-const {TwitterClient} = require('twitter-api-client');
+const { TwitterApi } = require('twitter-api-v2');
 
 const tweet = async (status) => {
-  const twitterClient = new TwitterClient({
-    apiKey: process.env.TWITTER_API_KEY,
-    apiSecret: process.env.TWITTER_API_SECRET,
-    accessToken: process.env.TWITTER_API_ACCESS_TOKEN,
-    accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-  });
 
-  await twitterClient.tweets.statusesUpdate({status});
+    const twitterClient = new TwitterApi({
+        appKey: process.env.TWITTER_API_KEY,
+        appSecret: process.env.TWITTER_API_SECRE,
+        // Following access tokens are not required if you are
+        // at part 1 of user-auth process (ask for a request token)
+        // or if you want a app-only client (see below)
+        accessToken: process.env.TWITTER_API_ACCESS_TOKEN,
+        accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    });
+  
+  await twitterClient.v2.tweet(status);
 };
 
 (async () => {
   try {
-    const myTweet = 
-          `Jab from the bot`;
+    const myTweet = 'Jab from the bot';
 
     await tweet(myTweet);
   } catch (err) {
